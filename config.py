8bd693d157070abc59cfc6c02c0beb3346ec5ca2
UFC_UPDATES <ufcupdates313@gmail.com>
2025-12-30 20:20:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,6 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 API_URL = getenv("API_URL", 'https://tgapi.xbitcode.com') #youtube song url
-VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
 API_KEY = getenv("API_KEY", 'xbit_M79PCh3BWqCHuXxDagWV5jfNrZBKjd7p')
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
